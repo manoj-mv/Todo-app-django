8b725b61470856b6ea55cc22a79041ae5b5e6675
Manoj MV <manojmv3019@gmail.com>
2020-12-12 12:13:57 +0530

    implemented task completion using ajax

--- tasks/urls.py
+++ tasks/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('',views.CreateTask_and_list.as_view(),name="create_and_list"),
     path('todo/<int:pk>/update',views.updateTask.as_view(),name='task_update'),
     path('todo/<int:pk>/delete',views.deleteTask.as_view(),name='task_delete'),
+    path('task_complete/<int:pk>',views.TaskComplete.as_view(),name='task_complete'),
+    path('task_incomplete/<int:pk>',views.TaskIncomplete.as_view(),name='task_incomplete'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
-from django.views.generic import ListView,CreateView,UpdateView,DeleteView
+from django.views.generic import ListView,CreateView,UpdateView,DeleteView,View
 from django.forms import ModelForm
 from django.urls import reverse_lazy
 
@@ -25,13 +25,34 @@ class CreateTask_and_list(CreateView):
 
 class updateTask(UpdateView):
     model=task
-    fields='__all__'
+    fields=['title']
     success_url=reverse_lazy('tasks:create_and_list')
 
 class deleteTask(DeleteView):
     model=task
     success_url=reverse_lazy('tasks:create_and_list')
 
+class TaskComplete(View):
+    model=task
+    def post(self,request,pk):
+        print("task_complete request")
+        task = get_object_or_404(self.model,pk=pk)
+        print(task)
+        if(task.complete == False):
+            task.complete = True
+            task.save()
+        return HttpResponse()
+
+class TaskIncomplete(View):
+    model=task
+    def post(self,request,pk):
+        print("task_incomplete request")
+        task = get_object_or_404(self.model,pk=pk)
+        print(task)
+        if(task.complete == True):
+            task.complete = False
+            task.save()
+        return HttpResponse()
 
 
 
